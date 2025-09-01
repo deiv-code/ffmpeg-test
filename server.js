cbd2cf9403600cd6f6ae3d639d9bae446deb8def
DavidDume <dumedavid11@gmail.com>
2025-09-01 18:28:56 +0300

    fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const TextGlowProcessor = require('./create_glow_text');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 // Middleware
 app.use(express.static('public'));
